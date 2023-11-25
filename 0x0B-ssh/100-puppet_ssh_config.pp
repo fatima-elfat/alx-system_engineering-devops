9870b88c26c6ce449376481ba2c10adfe160eef2
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-25 07:44:46 -0800

    fix errors

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,15 @@
 #  configuration file task.
-file { 'etc/ssh/ssh_config':
+#  file_line error add import stdlib
+include 'stdlib'
+file_line { 'PasswordAuthentication':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  content => 'Host *
-      PasswordAuthentication no
-      IdentifyFile ~/.ssh/school',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+file_line { 'IdentityFile':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
