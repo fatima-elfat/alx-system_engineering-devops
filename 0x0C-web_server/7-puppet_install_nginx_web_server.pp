916e1dfd239cf1797542ec6959ddee61df3e8fb7
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-28 10:47:42 -0800

    add files

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+#  configuration file task.
+#  file_line error add import stdlib
+include 'stdlib'
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
