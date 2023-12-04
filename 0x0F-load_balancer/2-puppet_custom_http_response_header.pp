103191662395b27953d2dec31963d290d0da748d
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-04 12:01:28 -0800

    fix errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,44 @@
 #  configuration file task.
 include 'stdlib'
-exec {'update':
-  command => '/usr/bin/apt-get update',
+
+exec { 'update':
+  command => '/usr/bin/apt-get update'
+}
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  require => Exec['update']
+}
+exec { 'restart':
+  command => '/usr/sbin/service nginx restart',
+  require => Package['nginx']
 }
 package { 'nginx':
   ensure => installed,
+  require => Exec['install Nginx']
 }
--> file_line { 'header':
-  path  => '/etc/nginx/sites-enabled/default',
-  after  => '/server_name',
-  line  => "_/a add_header X-Served-By \"${hostname}\";",
+
+file_line { 'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
 }
--> exec {'run':
-  command => '/usr/sbin/service nginx restart',
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+file_line { 'header':
+  ensure   => 'present',
+  after    => 'http {',
+  path     => '/etc/nginx/nginx.conf',
+  multiple => true,
+  line     => 'add_header X-Served-By \"${hostname}\";',
+  notify   => Exec['restart'],
 }
