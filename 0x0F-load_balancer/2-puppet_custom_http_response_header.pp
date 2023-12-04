d12e714b3c4313fa2d59e7e408ad6464ee85d61d
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-04 12:12:33 -0800

    fix errors

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,40 +5,23 @@ exec { 'update':
   command => '/usr/bin/apt-get update'
 }
 
-exec {'install Nginx':
+exec {'install':
   provider => shell,
   command  => 'sudo apt-get -y install nginx',
   require => Exec['update']
 }
 exec { 'restart':
-  command => '/usr/sbin/service nginx restart',
+  provider => shell,
+  command  => 'sudo service nginx restart',
   require => Package['nginx']
 }
 package { 'nginx':
   ensure => installed,
-  require => Exec['install Nginx']
+  require => Exec['install']
 }
-
-file_line { 'redirect':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
-file_line { 'header':
-  ensure   => 'present',
-  after    => 'http {',
-  path     => '/etc/nginx/nginx.conf',
-  multiple => true,
-  line     => 'add_header X-Served-By \"${hostname}\";',
-  notify   => Exec['restart'],
+exec { 'header':
+  provider    => shell,
+  environment => ["tmph=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$tmph\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart'],
 }
