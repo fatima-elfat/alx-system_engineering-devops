f75850b3aafb88398e919601f3ca5b0ce2916da8
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 00:23:44 +0100

    change file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,24 +8,30 @@ import requests
 
 
 if __name__ == "__main__":
+    USERS = []
+    TASKS = []
+    DATA = dict()
     URL = "http://jsonplaceholder.typicode.com"
-    users = requests.get("{}/users".format(URL)).json()
     filename = "todo_all_employees.json"
+    users = requests.get("{}/users".format(URL)).json()
+    todos = requests.get("{}/todos".format(URL)).json()
+    for u in users:
+        USERS.append((u.get('id'), u.get('username')))
+    for t in todos:
+        TASKS.append((t.get('userId'),
+                      t.get('title'),
+                      t.get('completed')))
+    for user in USERS:
+        todo = []
+        user = list(user)
+        for task in TASKS:
+            task =list(task)
+            if task[0] == user[0]:
+                todo.append({
+                        "username": user[1],
+                        "task": task[1],
+                        "completed": task[2]
+                        })
+        DATA[str(user[0])] = todo
     with open(filename, "w") as jsonfile:
-        tlist = []
-        datas = {}
-        for user in users:
-            USERNAME = user.get("username")
-            USERID = user.get("id")
-            todos = requests.get("{}/todos?userId={}".format(
-                                                            URL,
-                                                            USERID
-                                                            )).json()
-            for task in todos:
-                data = {"username": USERNAME,
-                        "task": task.get("title"),
-                        "completed": task.get("completed")
-                        }
-                tlist.append(data)
-            datas[USERID] = tlist
-        json.dump(datas, jsonfile)
+        json.dump(DATA, jsonfile, sort_keys=True)
