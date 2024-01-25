c089846fcc4b9b9d1f095f15e88a91da36849610
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-25 23:22:54 +0000

    fix  errors

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,11 @@
-#increase for hard and soft files
-$myvar = '/etc/security/limits.conf'
+#increase for hard and soft
 exec {'hard incr':
   provider => shell,
-  command  => 'sudo sed -i "s/ile 5/ile 50000/" $myvar',
+  command  => 'sudo sed -i "s/ile 5/ile 50000/" /etc/security/limits.conf',
   before   => Exec['soft incr'],
 }
 
 exec {'soft incr':
   provider => shell,
-  command  => 'sudo sed -i "s/ile 4/ile 40000/" $myvar',
+  command  => 'sudo sed -i "s/ile 4/ile 40000/" /etc/security/limits.conf',
 }
